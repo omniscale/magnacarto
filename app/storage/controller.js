e069679fb850f0132d87f19b7f7250a4d65f11ac
Kai Culemann <culemann@omniscale.de>
2015-06-26 11:25:15 +0200

    Handle grid init and grid update correct in storeage controller

--- app/storage/controller.js
+++ app/storage/controller.js
@@ -40,10 +40,19 @@ angular.module('magna-app')
       $cookieStore.put('savedMaps', $scope.maps);
     };
 
-    $scope.$on('gridster-item-initialized', function(){
-      $timeout(function(){
-        $scope.$broadcast('gridUpdate');
-      });
+    $scope.$watch(function() {
+      return angular.element(document.querySelector('.gridster-element')).attr('class');
+    }, function(classes){
+      if ((classes.indexOf('gridster-loaded')) > -1) {
+        // trigger updateSize in ol3-directive
+        $scope.$broadcast('gridInit');
+
+        $scope.$on('gridster-item-initialized', function(){
+          $timeout(function(){
+            $scope.$broadcast('gridUpdate');
+          }, 0);
+        });
+      }
     });
 
   }
