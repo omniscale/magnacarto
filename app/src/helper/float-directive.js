764a6969096f66f9fc5c56f3bdfeb78d3c7cbb3a
Kai Culemann <culemann@omniscale.de>
2015-07-29 13:12:00 +0200

    Added float validator

---
+++ app/src/helper/float-directive.js
@@ -0,0 +1,29 @@
+angular.module('magna-app')
+
+.directive('float', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      ctrl.$parsers.unshift(function(viewValue) {
+        if (!viewValue || viewValue.length === 0) {
+          ctrl.$setValidity('float', true);
+          return viewValue;
+        } else if (/^\-?\d*((\.|\,)\d+)?$/.test(viewValue)) {
+          //allow . and ,
+          var replacement = viewValue.replace(',', '.');
+          //allow - as single char
+          if('-' === replacement) {
+            ctrl.$setValidity('float', false);
+            return viewValue;
+          } else {
+            ctrl.$setValidity('float', true);
+            return replacement;
+          }
+        } else {
+          ctrl.$setValidity('float', false);
+          return undefined;
+        }
+      });
+    }
+  };
+});
