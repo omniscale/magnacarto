a353b5bd396c7eb71d02d798541c39d6c48747e3
Kai Culemann <culemann@omniscale.de>
2015-07-29 17:12:00 +0200

    Removed ng-sortable customization

--- app/libs/ng-sortable.js
+++ app/libs/ng-sortable.js
@@ -682,9 +682,7 @@
 
             dragElement = angular.element($document[0].createElement(scope.sortableScope.element.prop('tagName')))
               .addClass(scope.sortableScope.element.attr('class')).addClass(sortableConfig.dragClass);
-            // Magnacarto customization
-            dragElement.css('min-width', $helper.width(scope.itemScope.element) + 'px');
-            // end
+            dragElement.css('width', $helper.width(scope.itemScope.element) + 'px');
             dragElement.css('height', $helper.height(scope.itemScope.element) + 'px');
 
             placeHolder = createPlaceholder(scope.itemScope)
