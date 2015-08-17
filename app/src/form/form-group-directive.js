eaa0a3d2d30f80d2c45253e7456ace119377d27d
Kai Culemann <culemann@omniscale.de>
2015-08-17 15:00:34 +0200

    app: display correct error message

--- app/src/form/form-group-directive.js
+++ app/src/form/form-group-directive.js
@@ -13,7 +13,6 @@ angular.module('magna-app')
         'has-error': undefined,
         'has-warning': undefined
       };
-      scope.error = {};
 
       var items = element.find('input');
       if (items.length === 0) {
@@ -33,9 +32,8 @@ angular.module('magna-app')
       scope.id = item.attr('id') || scope.name;
 
       scope.itemScope = scope.form[scope.name];
-      scope.$watchGroup(['itemScope.$dirty', 'itemScope.$invalid', 'itemScope.$error'],
+      scope.$watchGroup(['itemScope.$dirty', 'itemScope.$invalid'],
         function (n, o, scope) {
-          scope.error.required = n[0] && n[2].required;
           scope.formClass['has-error'] = n[0] && n[1];
           scope.formClass['has-warning'] = !n[0] && n[1];
       });
