650af7021038570cf9a468e067a82bf7f9ae76eb
Kai Culemann <culemann@omniscale.de>
2015-08-17 11:04:32 +0200

    app: added form-group directive

---
+++ app/src/form/form-group-directive.js
@@ -0,0 +1,41 @@
+angular.module('magna-app')
+
+.directive('formGroup', ['$filter', function($filter) {
+  return {
+    // replace: true,
+    transclude: true,
+    require: '^form',
+    scope: {},
+    templateUrl: 'src/form/form-group.html',
+    link: function(scope, element, attrs, formController) {
+      scope.form = formController;
+
+      var items = element.find('input');
+      if (items.length === 0) {
+        items = element.find('select');
+      }
+      if(items.length === 0) {
+        throw 'NoInputElementError';
+      }
+
+      var item = angular.element(items[0]);
+      item.addClass('form-control');
+      scope.name = item.attr('name');
+      scope.title = item.attr('title') || $filter('titleCase')(scope.name);
+      scope.id = item.attr('id') || scope.name;
+
+      scope.itemScope = scope.form[scope.name];
+
+      scope.formClass = {
+        'has-error': undefined,
+        'has-warning': undefined
+      };
+
+      scope.$watchGroup(['itemScope.$touched', 'itemScope.$invalid'],
+        function (n, o, scope) {
+          scope.formClass['has-error'] = n[0] && n[1];
+          scope.formClass['has-warning'] = !n[0] && n[1];
+      });
+    }
+  };
+}]);
