8e00558a75335d640916e82b6f64b39a77d6d650
Oliver Tonnhofer <olt@bogosoft.com>
2016-02-23 17:34:31 +0100

    extend tests for mapserver

--- builder/mapserver/mapfile_test.go
+++ builder/mapserver/mapfile_test.go
@@ -3,8 +3,70 @@ package mapserver
 import (
 	"testing"
 
+	"github.com/omniscale/magnacarto/color"
+
+	"github.com/omniscale/magnacarto/config"
+	"github.com/omniscale/magnacarto/mml"
+	"github.com/omniscale/magnacarto/mss"
+
 	"github.com/stretchr/testify/assert"
 )
+
+var locator = config.LookupLocator{}
+
+func TestNoLayers(t *testing.T) {
+	m := New(&locator)
+	m.SetNoMapBlock(true)
+
+	assert.Empty(t, m.String())
+}
+
+func TestLineStringLayer(t *testing.T) {
+	m := New(&locator)
+	m.SetNoMapBlock(true)
+
+	m.AddLayer(mml.Layer{ID: "test", SRS: "4326", Type: mml.LineString},
+		[]mss.Rule{
+			{Layer: "test", Properties: mss.NewProperties(
+				"line-width", 1.0,
+				"line-color", color.MustParse("red"),
+				"line-opacity", 0.5,
+				"line-dasharray", []mss.Value{3.0, 5.0},
+			)},
+		})
+	result := m.String()
+	assert.Contains(t, result, "WIDTH 1\n")
+	assert.Contains(t, result, "COLOR \"#ff0000\"\n")
+	assert.Contains(t, result, "OPACITY 50\n")
+	assert.Regexp(t, `PATTERN\s+3\s+5\s+END`, result)
+}
+
+func TestPolygonLayer(t *testing.T) {
+	m := New(&locator)
+	m.SetNoMapBlock(true)
+
+	m.AddLayer(mml.Layer{ID: "test", SRS: "4326", Type: mml.Polygon},
+		[]mss.Rule{
+			{Layer: "test", Properties: mss.NewProperties(
+				"line-width", 1.0,
+				"line-color", color.MustParse("red"),
+				"line-opacity", 0.5,
+				"line-dasharray", []mss.Value{3.0, 5.0},
+				"polygon-fill", color.MustParse("blue"),
+				"polygon-opacity", 0.2,
+				"text-size", 10.0,
+				"text-name", []mss.Value{mss.Field("name")},
+			)},
+		})
+	result := m.String()
+	assert.Contains(t, result, "WIDTH 1\n")
+	assert.Contains(t, result, "OUTLINECOLOR \"#ff000080\"\n")
+	assert.Regexp(t, `PATTERN\s+3\s+5\s+END`, result)
+	assert.Contains(t, result, "COLOR \"#0000ff\"\n")
+	assert.Contains(t, result, "OPACITY 20\n")
+	assert.Regexp(t, `LABEL\s+ SIZE 7.4\d+`, result)
+	assert.Regexp(t, `TEXT 'name'`, result)
+}
 
 func TestItem(t *testing.T) {
 	assert.Equal(t, `KEY "str"`, Item{"key", quote("str")}.String())
