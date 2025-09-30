19b8fc5eebaa2a3cf94c35809e26d335c677122f
Oliver Tonnhofer <olt@bogosoft.com>
2025-09-30 09:10:54 +0200

    test: add custom helper to compare colors (fixes float comparsion errors)

--- color/color_test.go
+++ color/color_test.go
@@ -62,39 +62,39 @@ func TestMustParseColor(t *testing.T) {
 }
 
 func TestFunctionsColor(t *testing.T) {
-	assert.Equal(t, Color{5.0, 0.85, 0.5, 1.0, false}, Lighten(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
-	assert.Equal(t, Color{13.309706722717369, 0.9581840942803506, 0.5090338151746395, 1.0, true}, LightenP(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
-	assert.Equal(t, Color{5.0, 0.85, 0.4, 1.0, false}, Darken(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
-	assert.Equal(t, Color{13.309706722717369, 0.9581840942803506, 0.4090338151746395, 1.0, true}, DarkenP(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
+	assertEqualColor(t, Color{5.0, 0.85, 0.5, 1.0, false}, Lighten(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
+	assertEqualColor(t, Color{13.309706722717369, 0.9581840942803506, 0.5090338151746395, 1.0, true}, LightenP(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
+	assertEqualColor(t, Color{5.0, 0.85, 0.4, 1.0, false}, Darken(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
+	assertEqualColor(t, Color{13.309706722717369, 0.9581840942803506, 0.4090338151746395, 1.0, true}, DarkenP(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
 
-	assert.Equal(t, Color{5.0, 0.9, 0.45, 1.0, false}, Saturate(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
-	assert.Equal(t, Color{13.309706722717369, 1.0, 0.4590338151746395, 1.0, true}, SaturateP(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
-	assert.Equal(t, Color{5.0, 0.75, 0.45, 1.0, false}, Desaturate(Color{5.0, 0.8, 0.45, 1.0, false}, .05))
-	assert.Equal(t, Color{13.536246577831788, 0.8851800810869034, 0.4517462301819672, 1.0, true}, DesaturateP(Color{5.0, 0.8, 0.45, 1.0, false}, .05))
+	assertEqualColor(t, Color{5.0, 0.9, 0.45, 1.0, false}, Saturate(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
+	assertEqualColor(t, Color{13.309706722717369, 1.0, 0.4590338151746395, 1.0, true}, SaturateP(Color{5.0, 0.85, 0.45, 1.0, false}, .05))
+	assertEqualColor(t, Color{5.0, 0.75, 0.45, 1.0, false}, Desaturate(Color{5.0, 0.8, 0.45, 1.0, false}, .05))
+	assertEqualColor(t, Color{13.536246577831788, 0.8851800810869034, 0.4517462301819672, 1.0, true}, DesaturateP(Color{5.0, 0.8, 0.45, 1.0, false}, .05))
 
-	assert.Equal(t, Color{0.0, 0.0, 0.5, 0.05, false}, FadeIn(Color{0.0, 0.0, 0.5, 0.0, false}, .05))
-	assert.Equal(t, Color{0.0, 0.0, 0.5, 0.5, false}, FadeIn(Color{0.0, 0.0, 0.5, 0.0, false}, .50))
-	assert.Equal(t, Color{0.0, 0.0, 0.5, 0.7, false}, FadeIn(Color{0.0, 0.0, 0.5, 0.2, false}, .50))
+	assertEqualColor(t, Color{0.0, 0.0, 0.5, 0.05, false}, FadeIn(Color{0.0, 0.0, 0.5, 0.0, false}, .05))
+	assertEqualColor(t, Color{0.0, 0.0, 0.5, 0.5, false}, FadeIn(Color{0.0, 0.0, 0.5, 0.0, false}, .50))
+	assertEqualColor(t, Color{0.0, 0.0, 0.5, 0.7, false}, FadeIn(Color{0.0, 0.0, 0.5, 0.2, false}, .50))
 
-	assert.Equal(t, Color{0.0, 0.0, 0.5, 0.45, false}, FadeOut(Color{0.0, 0.0, 0.5, 0.5, false}, .05))
-	assert.Equal(t, Color{0.0, 0.0, 0.5, 0.0, false}, FadeOut(Color{0.0, 0.0, 0.5, 0.5, false}, .50))
-	assert.Equal(t, Color{0.0, 0.0, 0.5, 0.4, false}, FadeOut(Color{0.0, 0.0, 0.5, 0.9, false}, .50))
+	assertEqualColor(t, Color{0.0, 0.0, 0.5, 0.45, false}, FadeOut(Color{0.0, 0.0, 0.5, 0.5, false}, .05))
+	assertEqualColor(t, Color{0.0, 0.0, 0.5, 0.0, false}, FadeOut(Color{0.0, 0.0, 0.5, 0.5, false}, .50))
+	assertEqualColor(t, Color{0.0, 0.0, 0.5, 0.4, false}, FadeOut(Color{0.0, 0.0, 0.5, 0.9, false}, .50))
 
-	assert.Equal(t, Color{125.0, 0.85, 0.45, 1.0, false}, Spin(Color{5.0, 0.85, 0.45, 1.0, false}, 120))
-	assert.Equal(t, Color{133.30970672271738, 0.9581840942803506, 0.4590338151746395, 1.0, true}, SpinP(Color{5.0, 0.85, 0.45, 1.0, false}, 120))
+	assertEqualColor(t, Color{125.0, 0.85, 0.45, 1.0, false}, Spin(Color{5.0, 0.85, 0.45, 1.0, false}, 120))
+	assertEqualColor(t, Color{133.30970672271738, 0.9581840942803506, 0.4590338151746395, 1.0, true}, SpinP(Color{5.0, 0.85, 0.45, 1.0, false}, 120))
 
-	assert.Equal(t, Color{30.000000000000135, 0.05341979522184286, 0.7656000000000001, 1.0, false}, Multiply(Color{30, 0.364, 0.957, 1.0, false}, 0.8))
+	assertEqualColor(t, Color{30.000000000000135, 0.05341979522184286, 0.7656000000000001, 1.0, false}, Multiply(Color{30, 0.364, 0.957, 1.0, false}, 0.8))
 
-	assert.Equal(t, Color{124.22907488986785, 0.6387701928087549, 0.65458, 1.0, false}, Mix(Color{209, 0.81, 0.64, 1.0, false}, Color{109, 0.81, 0.64, 1.0, false}, 0.2))
-	assert.Equal(t, Color{203.38217261830283, 0.6419437751004016, 0.498, 1.0, false}, Mix(Color{180, 0.281, 0.496, 1.0, false}, Color{209.9, 1.0, 0.5, 1.0, false}, 0.5))
-	assert.Equal(t, Color{120, 1.0, 0.5, 1.0, false}, Mix(Color{0, 1.0, 0.5, 1.0, false}, Color{120, 1.0, 0.5, 1.0, false}, 0.0))
-	assert.Equal(t, Color{0, 1.0, 0.5, 1.0, false}, Mix(Color{0, 1.0, 0.5, 1.0, false}, Color{120, 1.0, 0.5, 1.0, false}, 1.0))
-	assert.Equal(t, Color{116.25, 1.0, 0.47058823529411764, 0.6800000000000002, false}, Mix(Color{0, 1.0, 0.5, 0.2, false}, Color{120, 1.0, 0.5, 0.8, false}, 0.2))
-	assert.Equal(t, Color{172.7974659092182, 0.7779747710280247, 0.6790756211469354, 1.0, true}, Mix(Color{109, 0.81, 0.64, 1.0, false}, Color{209, 0.81, 0.64, 1.0, true}, 0.2))
-	assert.Equal(t, Color{181.64356399470265, 0.7306791956543964, 0.6363175050747654, 1.0, true}, Mix(Color{109, 0.81, 0.64, 1.0, true}, Color{209, 0.81, 0.64, 1.0, true}, 0.2))
+	assertEqualColor(t, Color{124.22907488986785, 0.6387701928087549, 0.65458, 1.0, false}, Mix(Color{209, 0.81, 0.64, 1.0, false}, Color{109, 0.81, 0.64, 1.0, false}, 0.2))
+	assertEqualColor(t, Color{203.38217261830283, 0.6419437751004016, 0.498, 1.0, false}, Mix(Color{180, 0.281, 0.496, 1.0, false}, Color{209.9, 1.0, 0.5, 1.0, false}, 0.5))
+	assertEqualColor(t, Color{120, 1.0, 0.5, 1.0, false}, Mix(Color{0, 1.0, 0.5, 1.0, false}, Color{120, 1.0, 0.5, 1.0, false}, 0.0))
+	assertEqualColor(t, Color{0, 1.0, 0.5, 1.0, false}, Mix(Color{0, 1.0, 0.5, 1.0, false}, Color{120, 1.0, 0.5, 1.0, false}, 1.0))
+	assertEqualColor(t, Color{116.25, 1.0, 0.47058823529411764, 0.6800000000000002, false}, Mix(Color{0, 1.0, 0.5, 0.2, false}, Color{120, 1.0, 0.5, 0.8, false}, 0.2))
+	assertEqualColor(t, Color{172.7974659092182, 0.7779747710280247, 0.6790756211469354, 1.0, true}, Mix(Color{109, 0.81, 0.64, 1.0, false}, Color{209, 0.81, 0.64, 1.0, true}, 0.2))
+	assertEqualColor(t, Color{181.64356399470265, 0.7306791956543964, 0.6363175050747654, 1.0, true}, Mix(Color{109, 0.81, 0.64, 1.0, true}, Color{209, 0.81, 0.64, 1.0, true}, 0.2))
 
-	assert.Equal(t, Color{5.0, 0, 0.45, 1.0, false}, Greyscale(Color{5.0, 0.85, 0.45, 1.0, false}))
-	assert.Equal(t, Color{13.309706722717369, 0, 0.4590338151746395, 1.0, true}, GreyscaleP(Color{5.0, 0.85, 0.45, 1.0, false}))
+	assertEqualColor(t, Color{5.0, 0, 0.45, 1.0, false}, Greyscale(Color{5.0, 0.85, 0.45, 1.0, false}))
+	assertEqualColor(t, Color{13.309706722717369, 0, 0.4590338151746395, 1.0, true}, GreyscaleP(Color{5.0, 0.85, 0.45, 1.0, false}))
 
 	assert.Equal(t, 5.0, Hue(Color{5.0, 0.85, 0.45, 1.0, false}))
 	assert.Equal(t, 13.309706722717369, HueP(Color{5.0, 0.85, 0.45, 1.0, false}))
@@ -114,4 +114,12 @@ func TestSetHue(t *testing.T) {
 	hsl := MustParse("#996644")
 	hsl.H = MustParse("red").H
 	assert.Equal(t, hsl.String(), "#994444")
+}
+
+func assertEqualColor(t *testing.T, a, b Color) {
+	t.Helper()
+	assert.InDelta(t, a.H, b.H, 0.00001)
+	assert.InDelta(t, a.L, b.L, 0.00001)
+	assert.InDelta(t, a.S, b.S, 0.00001)
+	assert.InDelta(t, a.A, b.A, 0.00001)
 }
